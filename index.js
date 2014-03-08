c646df59cdd5159f20cc04cb3f17c64e8e307956
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-08 15:02:03 -0600

    [FEATURE] Add empty routes for creating, querying bridges.

--- index.js
+++ index.js
@@ -17,4 +17,22 @@ app.get('/wallet/new', function(req, res){
   });
 });
 
+app.post('/bridges', function(req, res){
+  /* 
+    link a dogecoin address to a ripple address
+    - create a user with email, password
+    - generate a dogecoin address
+    - create an external account for user with dogecoin address as name
+    - create a ripple address belong to the user
+  */
+});
+
+app.get('/bridges/dogecoin/:dogecoin_address', function(req, res){
+  // look up the connected ripple address given a dogecoin address
+});
+
+app.get('/bridges/ripple/:ripple_address', function(req, res){
+  // look up the connected dogecoin address given a ripple address
+});
+
 app.listen(5000);
