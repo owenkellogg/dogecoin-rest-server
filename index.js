e7f42cc081f82b796ef6f3184177a4921c360057
Steven Zeiler <zeiler.steven@gmail.com>
2014-03-09 20:29:41 -0500

    [FEATURE] Implement POST /bridges to connect ripple to dogecoin addresses.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')(); 
 var dogecoin = require("node-dogecoin")();
+var gateway = new (require("ripple-gateway-data-sequelize-adapter");
 
 dogecoin.auth(process.env.DOGECOIND_USER, process.env.DOGECOIND_PASSWORD);
 
@@ -18,16 +19,43 @@ app.get('/wallet/new', function(req, res){
 });
 
 app.post('/bridges', function(req, res){
-  /* 
-    link a dogecoin address to a ripple address
-    - create a user with email, password
-    - generate a dogecoin address
-    - create an external account for user with dogecoin address as name
-    - create a ripple address belong to the user
-  */
+
+  gateway.createUser({ name: req.body.user, password: req.body.password }, function(err, user){
+    if (err) {
+      res.send(500, { error: err });  
+    } else {
+      dogecoin.getNewAddress(function(err, address){
+        if (err) {
+          res.send(500, { error: err });  
+        } else {
+          gateway.createExternalAccount({ user_id: user.id, name: address }, function(err, account){
+            if (err) {
+              res.send(500, { error: err });  
+            } else {
+              gateway.createRippleAddress({ 
+                user_id: user.id, 
+                ripple_address: req.body.ripple_address          
+              }, function(err, ripple_address){
+                if (err) {
+                  res.send(500, { error: err });  
+                } else {
+                  res.send({ 
+                    user: user,
+                    ripple_address: ripple_address,
+                    dogecoin_address: account
+                  });
+                }                
+              });
+            } 
+          }); 
+        } 
+      });
+    } 
+  });
 });
 
 app.get('/bridges/dogecoin/:dogecoin_address', function(req, res){
+  
   // look up the connected ripple address given a dogecoin address
 });
 
